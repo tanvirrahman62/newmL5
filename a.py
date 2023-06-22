5f9dd99120d40a5f95bb97fe18a3aa17a086ae73
tanvirrahman62 <62463233+tanvirrahman62@users.noreply.github.com>
2023-06-22 14:06:37 +0600

    Add files via upload

---
+++ a.py
@@ -0,0 +1,139 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+# loading the diabetes dataset to a pandas DataFrame
+diabetes_dataset = pd.read_csv('C:/Users/Akib/Downloads/archive(2)/diabetes.csv') 
+
+pd.read_csv
+
+diabetes_dataset['Outcome'].value_counts()
+
+diabetes_dataset.groupby('Outcome').mean()
+
+# separating the data and labels
+X = diabetes_dataset.drop(columns = 'Outcome', axis=1)
+Y = diabetes_dataset['Outcome']
+
+scaler = StandardScaler()
+
+scaler.fit(X)
+
+standardized_data = scaler.transform(X)
+
+X = standardized_data
+Y = diabetes_dataset['Outcome']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
+
+classifier = svm.SVC(kernel='linear')
+
+#training the support vector Machine Classifier
+classifier.fit(X_train, Y_train)
+
+# accuracy score on the training data
+X_train_prediction = classifier.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+
+print('Accuracy score of the training data : ', training_data_accuracy)
+
+
+
+
+import pickle
+
+filename = 'diabetes.sav'
+pickle.dump(classifier, open(filename, 'wb'))
+
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+# loading the saved models
+
+diabetes_model = pickle.load(open('diabetes.sav', 'rb'))
+
+
+
+
+
+# sidebar for navigation
+with st.sidebar:
+    
+    selected = option_menu('Disease Prediction System',
+                          
+                          ['Diabetes Prediction'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+    
+    
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
+    
+    # page title
+    st.title('Diabetes Prediction using ML')
+    
+    
+    # getting the input data from the user
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+        
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+    
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+    
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+    
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+    
+    with col3:
+        BMI = st.text_input('BMI value')
+    
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    
+    with col2:
+        Age = st.text_input('Age of the Person')
+    
+    
+    # code for Prediction
+    diab_diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Diabetes Test Result'):
+        diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        
+        if (diab_prediction[0] == 1):
+          diab_diagnosis = 'The person is diabetic'
+        else:
+          diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
